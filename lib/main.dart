e9237907a87bb39cc875f12c1e2999303f519a9e
Fauzi <fauzi190198@gmail.com>
2022-01-14 22:23:34 +0700

    feat: add firebase + crashlytics

--- lib/main.dart
+++ lib/main.dart
@@ -31,8 +31,11 @@ import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 import 'package:ditonton/injection.dart' as di;
+import 'package:firebase_core/firebase_core.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
   di.init();
   runApp(MyApp());
 }
